c277e321b2c7e4ba3d20204d498cdb1de48ada70
Aashish <aashish.paliwal@live.com>
2025-09-28 00:39:26 +0530

    chore: add arbitrum testnet to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,7 @@ import '@openzeppelin/hardhat-upgrades';
 import 'solidity-coverage';
 import '@nomiclabs/hardhat-solhint';
 import '@primitivefi/hardhat-dodoc';
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -24,6 +25,14 @@ const config: HardhatUserConfig = {
     debugMode: true,
     outputDir: "./docgen",
     freshOutput: true,
+  },
+  networks: {
+    arbitrumTestnet: {
+      url: process.env.ARBITRUM_TESTNET_RPC_URL || '',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      timeout: 1200000
+    },
   },
 };
 
